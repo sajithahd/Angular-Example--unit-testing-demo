26a4f79933f9d9cd06fa0e81e9ae0bd272a1a856
sajitha <sajitha@codegen.net>
2021-01-17 14:56:26 +0530

    testing commit

---
+++ src/app/api/api.service.spec.ts
@@ -0,0 +1,58 @@
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
+import { TestBed } from '@angular/core/testing';
+import { Post } from '../models/post';
+import { APIService } from './api.service';
+
+describe('API service', () => {
+
+  let httpTestingController: HttpTestingController;
+  let apiService: APIService;
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+      providers: [APIService]
+    });
+
+    httpTestingController = TestBed.inject(HttpTestingController);
+    apiService = TestBed.inject(APIService);
+  });
+
+  afterEach(() => {
+    httpTestingController.verify();
+  });
+
+  describe('#getPosts', () => {
+    const posts: Post[] = [
+      {
+        userId: 1,
+        id: 1,
+        title: 'sunt aut facere repellat provident occaecati excepturi optio reprehenderit',
+        body: 'quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto'
+      },
+      {
+        userId: 1,
+        id: 2,
+        title: 'qui est esse',
+        body: 'est rerum tempore vitae\nsequi sint nihil reprehenderit dolor beatae ea dolores neque\nfugiat blanditiis voluptate porro vel nihil molestiae ut reiciendis\nqui aperiam non debitis possimus qui neque nisi nulla'
+      },
+      {
+        userId: 1,
+        id: 3,
+        title: 'ea molestias quasi exercitationem repellat qui ipsa sit aut',
+        body: 'et iusto sed quo iure\nvoluptatem occaecati omnis eligendi aut ad\nvoluptatem doloribus vel accusantium quis pariatur\nmolestiae porro eius odio et labore et velit aut'
+      }
+    ];
+
+    it('should return posts array', () => {
+      apiService.getPosts().subscribe(p => {
+        expect(p).toEqual(posts, 'should return expected posts');
+      });
+      const testRequest = httpTestingController.match('https://jsonplaceholder.typicode.com/posts');
+      expect(testRequest[0].request.method).toEqual('GET');
+      expect(testRequest.length).toEqual(1);
+      testRequest[0].flush(posts);
+    });
+
+  });
+
+});
