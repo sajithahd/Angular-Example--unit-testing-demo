196093595fbbac68e4f493c74a31173393d24745
sajitha <sajitha@codegen.net>
2021-01-18 00:32:03 +0530

    testing commit - service testing

--- src/app/components/posts/posts.component.spec.ts
+++ src/app/components/posts/posts.component.spec.ts
@@ -1,22 +1,47 @@
-import { HttpClient, HttpHandler } from '@angular/common/http';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-import { APIService } from '../../api/api.service';
+import { BehaviorSubject } from 'rxjs';
 import { FacadeService } from '../../facade.service';
-import { LoggerService } from '../../services/logger.service';
-import { StateService } from '../../state/state.service';
+import { Post } from '../../models/post';
 
 import { PostsComponent } from './posts.component';
 
 describe('PostsComponent', () => {
   let component: PostsComponent;
   let fixture: ComponentFixture<PostsComponent>;
+  let facadeServiceSpy: jasmine.SpyObj<FacadeService>;
+
+  const expectedPosts: Post[] = [
+    {
+      userId: 1,
+      id: 1,
+      title: 'sunt aut facere repellat provident occaecati excepturi optio reprehenderit',
+      body: 'quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto'
+    },
+    {
+      userId: 1,
+      id: 2,
+      title: 'qui est esse',
+      body: 'est rerum tempore vitae\nsequi sint nihil reprehenderit dolor beatae ea dolores neque\nfugiat blanditiis voluptate porro vel nihil molestiae ut reiciendis\nqui aperiam non debitis possimus qui neque nisi nulla'
+    },
+    {
+      userId: 1,
+      id: 3,
+      title: 'ea molestias quasi exercitationem repellat qui ipsa sit aut',
+      body: 'et iusto sed quo iure\nvoluptatem occaecati omnis eligendi aut ad\nvoluptatem doloribus vel accusantium quis pariatur\nmolestiae porro eius odio et labore et velit aut'
+    }
+  ];
 
   beforeEach(async () => {
+    const facadeServiceSpyValue = jasmine.createSpyObj('FacadeService', ['getPosts$', 'loadPosts']);
+
     await TestBed.configureTestingModule({
       declarations: [PostsComponent],
-      providers: [FacadeService, StateService, APIService, HttpClient, HttpHandler, LoggerService]
-    })
-                 .compileComponents();
+      providers: [{provide: FacadeService, useValue: facadeServiceSpyValue}]
+    }).compileComponents();
+
+    facadeServiceSpy = TestBed.inject(FacadeService) as jasmine.SpyObj<FacadeService>;
+    facadeServiceSpy.getPosts$.and.returnValue(new BehaviorSubject(expectedPosts));
+
   });
 
   beforeEach(() => {
@@ -27,5 +52,18 @@ describe('PostsComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should display posts', () => {
+
+    // fixture.whenStable().then(() => {
+    //   fixture.detectChanges();
+    //   const postContainer = fixture.nativeElement.querySelector('.post');
+    //   expect(postContainer.textContent).toContain('sunt aut facere');
+    // });
+
+    const postContainer = fixture.nativeElement.querySelector('.post');
+    expect(postContainer.textContent).toContain('sunt aut facere');
+
   });
 });
